3a600fce68a9cb962ffc34030bd2a1cb153c7b3c
hung.hoangmanh <hung.hoangmanh@vti.com.vn>
2019-01-08 16:04:12 +0700

    show list product

--- src/main/java/com/h2/mafia/controller/AbstractController.java
+++ src/main/java/com/h2/mafia/controller/AbstractController.java
@@ -2,8 +2,10 @@ package com.h2.mafia.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import com.h2.mafia.response.MafiaStoreResponse;
+import com.h2.mafia.utils.ResponseUtils;
 
-public class AbstractController {
+public class AbstractController extends ResponseUtils {
 	protected final Logger LOG = LoggerFactory.getLogger(getClass());
 	
 	protected static final String LIST = "list";
@@ -12,5 +14,7 @@ public class AbstractController {
 	protected static final String UPDATE = "update";
 	protected static final String DELETE = "delete/{id}";
 	
-	
+	protected MafiaStoreResponse success() {
+		return msgResponse(com.h2.mafia.common.RestCode.SUCCESS);
+	}
 }
